9b0f295caec5eccd31520ce3a3b15bbd75e93226
Ryland <rylandgrounds@gmail.com>
2021-03-05 11:35:49 -0600

    moving respository to github/initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let menuToggler = document.querySelector(".nav_button");
+let navLinks = document.querySelectorAll(".nav-link");
+let body = document.querySelector("body");
+
+
+menuToggler.addEventListener("click", () => {
+     body.classList.toggle("open");
+})
+
+
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=>{
+        body.classList.toggle("open");
+    })
+})
