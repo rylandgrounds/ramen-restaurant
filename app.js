fefe940cbf15a84d769ac5bb33268c098b7042ca
Ryland <rylandgrounds@gmail.com>
2021-03-09 20:09:18 -0600

    additional images, bug fixes, and styling

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-let menuToggler = document.querySelector(".nav_button");
+let menuToggler = document.querySelector(".nav__button");
 let navLinks = document.querySelectorAll(".nav-link");
 let body = document.querySelector("body");
 
-
 menuToggler.addEventListener("click", () => {
-     body.classList.toggle("open");
-})
-
+    body.classList.toggle("open");
+});
 
 navLinks.forEach(link => {
-    link.addEventListener('click', ()=>{
+    link.addEventListener("click", () => {
         body.classList.toggle("open");
-    })
-})
+    });
+});
 
-//Carousel
-$(document).ready(function() {
-$(".carousel").slick(
-    {
-        autoPlay: true,
-        autoPlaySpeed: 3000,
+// Slideshow
+$(document).ready(function () {
+    $(".carousel").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
         speed: 1000,
         dots: true
-    }
-)
-})
+    });
+});
